ecf87e4292d643d2e851139a7cc9adff3b01a1da
eoFranca <erfranca.com@gmail.com>
2024-03-01 09:26:12 -0300

    Criação da API Categoria.

--- app/urls.py
+++ app/urls.py
@@ -11,6 +11,13 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from core.views import UserViewSet
 
+from core.views import CategoriaViewSet # nova linha
+from core.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="users")
+router.register(r"categorias", CategoriaViewSet) # nova linha
+
 router = DefaultRouter()
 
 router.register(r"users", UserViewSet, basename="users")
