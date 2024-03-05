9cd594f5e4a11fb64553bb89f83bc743eedf88a1
eoFranca <erfranca.com@gmail.com>
2024-03-05 09:11:45 -0300

    Conectando com o Front-end.

--- app/urls.py
+++ app/urls.py
@@ -9,7 +9,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet
+
 
 from core.views import CategoriaViewSet # nova linha
 from core.views import UserViewSet
@@ -18,9 +18,9 @@ router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
 router.register(r"categorias", CategoriaViewSet) # nova linha
 
-router = DefaultRouter()
 
-router.register(r"users", UserViewSet, basename="users")
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
