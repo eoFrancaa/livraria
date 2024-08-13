e2275600d809acf5794bf2fb6ad897316dcbb8da
eoFrancaa <erfranca.com@gmail.com>
2024-08-13 09:10:45 -0300

    Adicionando capa para Livro.

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,11 +1,13 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 from core.models import Livro
 
+
 class LivroSerializer(ModelSerializer):
-    class Meta:
-        model = Livro
-        fields = "__all__"
+    capa_attachment_key = SlugRelatedField(source="capa",queryset=Image.objects.all(),slug_field="attachment_key",required=False,write_only=True,)
+    capa = ImageSerializer(required=False, read_only=True)
 
 
 class LivroDetailSerializer(ModelSerializer):
@@ -13,6 +15,7 @@ class LivroDetailSerializer(ModelSerializer):
         model = Livro
         fields = "__all__"
         depth = 1
+        capa = ImageSerializer(required=False)
 
 
 class LivroListSerializer(ModelSerializer):
